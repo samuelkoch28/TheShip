60f0087ba5d51c3c9e262a66ca7b634283062e31
Samuel Koch <samuel_koch@sluz.ch>
2024-08-29 13:39:36 +0200

    Created functions to automate buying and selling iron

--- jobs.py
+++ jobs.py
@@ -1,16 +1,19 @@
 import requests
+import asyncio
 from easySteering import *
 from communication import *
 from cargoHold import *
 
 
-def SellEverythingAtCoreStation():
-    stationsInReach = {}
-    flyToName("Core Station")
-    while(stationsInReach == {}):
-        stationsInReach = getStationsInReach().get('stations')
+async def SellEverythingAtCoreStation():
+    await flyToName("Core Station")
     cargoHold = getCargoHold()
     resources = cargoHold.get('hold').get('resources')
     for resourceName, amount in resources.items():
         sell("Core Station", resourceName, amount)
 
+async def BuyIron():
+    await flyToName("Vesta Station")
+    cargoHold = getCargoHold()
+    holdFree = cargoHold.get('hold').get('hold_free')
+    buy("Vesta Station", "IRON", holdFree)
